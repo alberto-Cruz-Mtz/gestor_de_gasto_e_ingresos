22e7221061197dea2120197fe222cfd40ae6bcfe
José Alberto Cruz Martínez <albertocruz8133@proton.me>
2025-06-16 15:40:03 -0600

    test realizado en nvim

--- src/test/java/com/example/backend/BackendApplicationTests.java
+++ src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.backend;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+  @Test
+  void contextLoads() {
+  }
 
+  @Test
+  void passing() {
+    System.out.println("testing");
+    assertTrue(true, "something is worng");
+  }
+
+  @Test
+  void failing() {
+    System.out.println("testing");
+    assertTrue(false, "something is wrong");
+  }
 }
